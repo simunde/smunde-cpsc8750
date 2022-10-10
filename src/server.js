8a371a833547ad3f9dfe49e29e9ae06cfa829e8f
simunde <sidmundece17@gmail.com>
2022-10-09 23:22:39 -0400

    Dynamic website and  cookies

--- src/server.js
+++ src/server.js
@@ -1,8 +1,13 @@
 // use the express library
 const express = require('express');
 
+//use cookie parser
+const cookieParser = require('cookie-parser');
+
 // create a new server application
 const app = express();
+
+app.use(cookieParser());
 
 // Define the port we will listen on
 // (it will attempt to read an environment global
@@ -11,13 +16,32 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-// The main page of our website
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-});
+let nextVisitorId = 1;
+var currTime = new Date();
 
+const {encode} = require('html-entities');
+app.use(express.static('public'));
+
+// ... snipped out code ...
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  res.cookie('visitorId', nextVisitorId);
+  res.cookie('visited', Date.now().toString());
+  res.render('welcome', {
+    name: req.query.name || "World",
+    date: req.query.date || new Date().toLocaleString(),
+    nextVisitorId: req.query.nextVisitorId || nextVisitorId,
+    visited: req.query.visited || Math.round((new Date().getTime() - currTime.getTime()) / 1000),
+  });
+
+  currTime = new Date();
+
+});
 // Start listening for network connections
 app.listen(port);
+
 
 // Printout for readability
 console.log("Server Started!");

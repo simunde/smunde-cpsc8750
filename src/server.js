0ebfffc4bc73f436b571660238a332786013f615
simunde <sidmundece17@gmail.com>
2022-10-30 20:47:17 -0400

    Added API Layer

--- src/server.js
+++ src/server.js
@@ -1,6 +1,8 @@
 // use the express library
 const express = require('express');
 
+const fetch = require('node-fetch');
+
 //use cookie parser
 const cookieParser = require('cookie-parser');
 
@@ -44,8 +46,10 @@ console.log(lasttimevisit);
   if(req.cookies['visitorId']){
   res.cookie('visitorId', nextVisitorId);}
   else{
-  res.cookie('visitorId', nextVisitorId++);
-}
+
+    res.cookie('visitorId', nextVisitorId++);
+
+  }
 
   res.cookie('visited', Date.now().toString());
   
@@ -59,6 +63,48 @@ console.log(lasttimevisit);
   currTime = new Date();
 
 });
+
+app.get("/trivia", async(req, res) => {
+    // fetch the data
+    const response = await fetch("https://opentdb.com/api.php?amount=1&type=multiple");
+
+    // fail if bad response
+    if (!response.ok) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with HTTP code ${response.status}`);
+        return;
+    }
+
+    // interpret the body as json
+    const content = await response.json();
+
+    // fail if db failed
+    if (content.response_code !== 0) {
+        res.status(500);
+        res.send(`Open Trivia Database failed with internal response code ${content.response_code}`);
+        return;
+    }
+
+    // respond to the browser
+    correctAnswer = content.results[0]['correct_answer']
+    answers = content.results[0]['incorrect_answers']
+    answers.push(correctAnswer)
+    let jumbledAnswers = answers.sort(function() {
+        return Math.random() - 0.5;
+    });
+    const answerLinks = jumbledAnswers.map(answer => {
+        return `<a style='color:white' href="javascript:alert('${
+          answer === correctAnswer ? 'Correct!' : 'Incorrect, Please Try Again!'
+          }')">${answer}</a>`
+    })
+    res.render('trivia', {
+        question: content.results[0]['question'],
+        category: content.results[0]['category'],
+        difficulty: content.results[0]['difficulty'],
+        answers: answerLinks
+    })
+});
+
 // Start listening for network connections
 app.listen(port);
 
